6b13f507492471e06c3215e980ab390fe784da24
Manfred Sommers <Manfred.Sommers@gmail.com>
2018-06-25 21:16:53 +0200

    Add files via upload

--- MNI.sol
+++ MNI.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'Mani' token contract
 //
 // Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Symbol      : MNI
+// Name        : Mani Token
 // Total supply: 100000000
 // Decimals    : 18
 //
